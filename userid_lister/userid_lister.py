a5bc30f86bbb08d5b5407d59b3a80771040b53b3
Sebastian Kuipers <kuipers.sebastian@ziggo.nl>
2022-12-17 13:02:26 +0100

    Created userid_lister plugin

---
+++ userid_lister/userid_lister.py
@@ -0,0 +1,19 @@
+from discord.ext import commands
+
+from core.checks import thread_only
+
+class Userid_lister(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    @thread_only()
+    async def userid(self, ctx):
+        await ctx.send(ctx.thread.recipient.id)
+
+    @commands.Cog.listener
+    async def on_thread_ready(self, thread, creator, category, initial_message):
+        await thread.channel.send(thread.recipient.id)
+
+async def setup(bot):
+    await bot.add_cog(Userid_lister(bot))
