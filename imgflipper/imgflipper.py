74ba042c9526ef64763aa6f530d338b90744ca22
Sebastian Kuipers <kuipers.sebastian@ziggo.nl>
2023-04-11 13:43:23 +0200

    Fix bug with bytes conversion

--- imgflipper/imgflipper.py
+++ imgflipper/imgflipper.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 
+from io import BytesIO
 from PIL import Image
 
 class ImgFlipper(commands.Cog):
@@ -17,10 +18,13 @@ class ImgFlipper(commands.Cog):
         if not attachment.content_type.startswith("image/"):
             await ctx.send("You need to attach an image!")
             return
-        image_bytes: bytes = await attachment.read()
-        image: Image.Image = Image.frombytes("RGBA", (attachment.height, attachment.width), image_bytes)
+        image_bytes: BytesIO = BytesIO(await attachment.read())
+        image: Image.Image = Image.open(image_bytes)
         flipped_image: Image.Image = image.transpose(Image.Transpose.FLIP_LEFT_RIGHT)
-        await ctx.send(file=discord.File(flipped_image.tobytes(), filename="flipped.png"))
+        to_send_bytes: BytesIO = BytesIO()
+        flipped_image.save(to_send_bytes, format="PNG")
+        to_send_bytes.seek(0)
+        await ctx.send(file=discord.File(to_send_bytes, filename="flipped.png"))
 
 async def setup(bot):
     await bot.add_cog(ImgFlipper(bot))
