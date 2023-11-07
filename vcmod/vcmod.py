12b583d892ea6af6ce960116b3c9b17f6e2a1ae3
Sebastian Kuipers <kuipers.sebastian@ziggo.nl>
2023-11-07 19:33:25 +0100

    Fix role loading

--- vcmod/vcmod.py
+++ vcmod/vcmod.py
@@ -14,6 +14,15 @@ class VcMod(commands.Cog):
         print(self.access_role)
         print(self.block_role)
 
+    async def cog_load(self):
+        await self.get_roles()
+
+    async def get_roles(self):
+        guild = self.bot.get_guild(685715989891121152)
+        await guild.fetch_roles()
+        self.access_role = guild.get_role(1171348301501706390)
+        self.block_role = guild.get_role(1171348301501706390)
+
     @checks.has_permissions(PermissionLevel.SUPPORTER)
     @commands.group(invoke_without_command=True)
     async def voicemod(self, ctx):
@@ -23,6 +32,8 @@ class VcMod(commands.Cog):
     @checks.has_permissions(PermissionLevel.SUPPORTER)
     @voicemod.command(name="give", aliases=["allow"])
     async def give(self, ctx, member: discord.Member):
+        if not self.allow_role:
+            await self.get_roles()
         await member.remove_roles(*self.block_role)
         await member.add_roles(*self.access_role)
         await ctx.send("Gave permissions to {member}")
@@ -30,6 +41,8 @@ class VcMod(commands.Cog):
     @checks.has_permissions(PermissionLevel.SUPPORTER)
     @voicemod.command(name="block", aliases=["disallow"])
     async def block(self, ctx, member: discord.Member):
+        if not self.allow_role:
+            await self.get_roles()
         await member.remove_roles(*self.access_role)
         await member.add_roles(*self.block_role)
         await ctx.send("Blocked permissions from {member}")
