a0b994869b4c4198b7905bcaf6f83e7787a1775c
Sebastian Kuipers <kuipers.sebastian@ziggo.nl>
2023-11-07 19:22:58 +0100

    Added vcmod plugin

---
+++ vcmod/vcmod.py
@@ -0,0 +1,33 @@
+from core import checks
+from core.models import PermissionLevel
+
+import discord
+from discord.ext import commands
+
+class VcMod(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        guild = self.bot.get_guild(685715989891121152)
+        self.access_role = guild.get_role(1171348301501706390)
+        self.block_role = guild.get_role(1171348301501706390)
+
+    @checks.has_permissions(PermissionLevel.SUPPORTER)
+    @commands.group(invoke_without_command=True)
+    async def voicemod(self, ctx):
+        """Voicemod help"""
+        await ctx.send_help(ctx.command)
+
+    @checks.has_permissions(PermissionLevel.SUPPORTER)
+    @voicemod.command(name="give", aliases=["allow"])
+    async def give(self, ctx, member: discord.Member):
+        await member.remove_roles(*self.block_role)
+        await member.add_roles(*self.access_role)
+
+    @checks.has_permissions(PermissionLevel.SUPPORTER)
+    @voicemod.command(name="give", aliases=["disallow"])
+    async def block(self, ctx, member: discord.Member):
+        await member.remove_roles(*self.access_role)
+        await member.add_roles(*self.block_role)
+
+async def setup(bot):
+    await bot.add_cog(VcMod(bot))
