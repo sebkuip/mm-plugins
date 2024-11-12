07ac29be04ae3d706029deddaebf2c8a47714886
Sebastian Kuipers <kuipers.sebastian@ziggo.nl>
2024-11-12 14:22:18 +0100

    Update to ignore mods

--- MNFH-dm-limiter/MNFH-dm-limiter.py
+++ MNFH-dm-limiter/MNFH-dm-limiter.py
@@ -8,9 +8,11 @@ class MNFHDMLimiter(commands.Cog):
     async def on_message(self, message):
         if message.author.bot:
             return
-        elif not message.channel.id in (721750845749723236, 1065831118970040340):
+        elif message.channel.id not in (721750845749723236, 1065831118970040340):
             return
-        elif len(message.content) > 80:
+        elif message.author.guild_permissions.manage_messages:
+            return
+        elif len(message.content) > 100:
             await message.reply("This channel is only for asking people to DM with you. Intro's or other long messages are not allowed.", delete_after=10)
             await message.delete()
 
